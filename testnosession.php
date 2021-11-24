f9bd8227374ff133e84408fcb2e93cf60aa2a5fb
KHAMPEERADA P <62010388@kmitl.ac.th>
2021-11-24 23:24:21 +0700

    -

---
+++ testnosession.php
@@ -0,0 +1,89 @@
+<title>Home | Website</title>
+<html lang="en">
+<head>    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- add icon link -->
+    <link rel="icon" href="images/icon.jpg" type="image/x-icon">
+    <!-- specifying a webpage icon for web clip -->
+    <link rel="apple-touch-icon" href="images/icon.jpg" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+
+        <div class="container">
+          <a class="navbar-brand" href="#">เรด-DIT</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse ms-auto justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" id="forum" href="forum.php">Forum</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+              <div class="dropdown">
+                <button class="dropbtn">                  
+                </button>
+                <div class="dropdown-content">
+                  <a href="profile.php">Profile</a>
+                  <a href="logout.php">Log Out</a>
+                </div>
+              </div>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+<body>
+    <div class="form-popup" id="myForm" style="z-index: 1000;">
+    <form action="login.php" class="form-container" method="POST">
+        <h1>Login</h1>
+
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+
+        <button type="submit" class="btn" name="user_login">Login</button>
+        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    </form>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">เรด-DIT</a>
+        </div>
+      </nav>
+      <script type="module" src="js/index.js"></script>
+
+
+<section class="main frame" style="position: relative; top: 100px; width: 100px; margin-left: 50px; z-index:1400;">
+<div class="row">
+  <p class="latest header" style="font-size: 36px; border: 2px dashed black;">Latest</p>
+</div>
+</section>  
+</body>
+</html>
+
+<script src="js/main.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.js'></script>
+<script src='https://threejs.org/examples/js/controls/TrackballControls.js'></script>
+<script src='https://mamboleoo.be/learnThree/demos/OBJLoader.js'></script>
+    <script>
+      function closeForm() {
+          document.getElementById("myForm").style.display = "none";
+        }
+    </script>
+</html>
