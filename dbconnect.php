469e95deff961b74d8b8030be89801d4029af24f
KHAMPEERADA P <62010388@kmitl.ac.th>
2021-11-25 00:19:12 +0700

    -

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,16 @@
 <?php
     error_reporting(-1);
-    //$servername = "localhost";
+    $servername = "localhost";
     //$username = "root";
     //$password = "";
-    $servername = "34.124.243.161:3366";
+    //$servername = "34.124.243.161:3366";
     $username = "ite";
     $password = "ite@@#2021";
     $dbname = "project";
 
         // Create Connection
-        $connect = mysqli_connect($servername, $username, $password, $dbname,3306);
-
+        $connect = mysqli_connect($servername, $username, $password, $dbname);
+        //$connect = mysqli_connect($servername, $username, $password, $dbname,3306);
         // Check connection
         if (!$connect) {
             die("Connection failed" . mysqli_connect_error());
