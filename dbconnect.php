1273cd90db304da07e15542365692d22d072178d
KHAMPEERADA P <62010388@kmitl.ac.th>
2021-11-25 01:19:04 +0700

    -

--- dbconnect.php
+++ dbconnect.php
@@ -1,20 +1,19 @@
 <?php
     error_reporting(-1);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    //$servername = "34.124.243.161:3306";
-    //$username = "ite";
-    //$password = "ite@@#2021";
+    //$servername = "localhost:3306";
+    //$username = "root";
+    //$password = "";
+    $servername = "34.124.243.161:3306";
+    $username = "ite";
+    $password = "ite@@#2021";
     $dbname = "project";
 
         // Create Connection
-        $connect = mysqli_connect($servername, $username, $password, $dbname);
+        $connect = mysqli_connect($servername, $username, $password, $dbname,3306);
         // Check connection
         if (!$connect) {
             echo mysqli_connect_error();
             die("Connection failed" . mysqli_connect_error());
             //หรือย่อเป็น $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed");
-        } 
-    
+        }   
 ?>
--- header.php
+++ header.php
@@ -17,7 +17,7 @@ session_start();
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="index.php">WEB NAME</a>
+          <a class="navbar-brand" href="index.php">เรด-DIT</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
